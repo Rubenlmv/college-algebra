d5aa0b3587ddd61ff2c64c9abdac2d39e2bff9e3
Rubenlmv <ruben_limbert@hotmail.com>
2026-07-13 01:43:18 +0000

    Latest build deployed.

--- web/lunr-pretext-search-index.js
+++ web/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Syllabus",
-  "body": " Syllabus        Course Information  This is the syllabus for course name (MATH xxx, section xxx) for [term] 20xx. It is a [n] credit course.    Instructor  Prof. Lastname, Office Location, prof.lastname@example.edu .    Student Hours  TBD    Class meets  course times and location.    Course Description  course description from catalog    Prerequisite  list of prerequisites    Textbook and course materials   textbook name by textbook author.       Course Overview        Assessments and Grades     "
+  "body": " Syllabus        Course Information  Testing changes. This is the syllabus for course name (MATH xxx, section xxx) for [term] 20xx. It is a [n] credit course.    Instructor  Prof. Lastname, Office Location, prof.lastname@example.edu .    Student Hours  TBD    Class meets  course times and location.    Course Description  course description from catalog    Prerequisite  list of prerequisites    Textbook and course materials   textbook name by textbook author.       Course Overview        Assessments and Grades     "
 },
 {
   "id": "sec-course-info-2",
@@ -37,6 +37,51 @@ var ptx_lunr_docs = [
   "body": " Week 2   Monday      Wednesday      Friday     "
 },
 {
+  "id": "section-1-1",
+  "level": "1",
+  "url": "section-1-1.html",
+  "type": "Section",
+  "number": "",
+  "title": "Math 1320 Section 1.1",
+  "body": " Math 1320 Section 1.1   Idea 1 – Linear Equations  Linear equations are also known as equations of first degree. That is, variables have no exponents .  In General, this would exclude equations with variables in the denominators of fractions, since they would have an exponent of . However, we will see some equations with variables in their denominators that are equivalent to linear equations.    Idea 2 – Solving Equations  To solve an equation means to find all possible solutions.  Good News! When solving an equation you can do anything you want—as long as you do it twice. Once to the whole left side and once to the whole right side.  The most common things we do:  Add\/subtract same value from both sides  Multiply\/divide both sides by the same non-zero value    We solve equations by:  Distribute to eliminate parentheses  Combine \"Like Terms\"  Use Addition\/Subtraction to \"collect\" variables on one side, constants on the other  Use Multiplication\/Division to isolate the variable      Solving Linear Equations Video  Watch the video below for a step-by-step explanation:      Example    Solve:     Step 1: Simplify the right side    Step 2: Collect variables on one side    Step 3: Isolate the variable   Check:      Therefore, the solution is .      Clearing Fractions    To clear fractions from an equation:  Find the Lowest Common Denominator (LCD) of ALL the fractions. (If you can't find the lowest common denominator, just multiply all denominators together.)  Multiply every term (on BOTH sides) by the LCD  Simplify each term. There should be no fractions.  Solve the new equation using your algebra skills.      Example    Solve:     LCD =  Step 1: Multiply every term by 30   Step 2: Simplify   Step 3: Solve     Therefore, the solution is .      What happens if there is a variable in the denominator?     Solve:   LCD: Restricted Values for :     LCD =  Restricted Values: (because is in the denominator)  Step 1: Multiply every term by   Step 2: Simplify   Step 3: Solve     Since , this is a valid solution.  Check:       Therefore, the solution is .       Solve:   LCD: Restricted Values for :     LCD =  Restricted Values: and  Step 1: Multiply every term by   Step 2: Simplify each term         Step 3: Factor     Since is a restricted value (it makes ), we discard it.  Therefore, the solution is:      "
+},
+{
+  "id": "example-linear-equation-2",
+  "level": "2",
+  "url": "section-1-1.html#example-linear-equation-2",
+  "type": "Example",
+  "number": "1",
+  "title": "",
+  "body": "  Solve:     Step 1: Simplify the right side    Step 2: Collect variables on one side    Step 3: Isolate the variable   Check:      Therefore, the solution is .   "
+},
+{
+  "id": "example-clearing-fractions-2",
+  "level": "2",
+  "url": "section-1-1.html#example-clearing-fractions-2",
+  "type": "Example",
+  "number": "2",
+  "title": "",
+  "body": "  Solve:     LCD =  Step 1: Multiply every term by 30   Step 2: Simplify   Step 3: Solve     Therefore, the solution is .   "
+},
+{
+  "id": "ex-denominator-1",
+  "level": "2",
+  "url": "section-1-1.html#ex-denominator-1",
+  "type": "Example",
+  "number": "3",
+  "title": "",
+  "body": "  Solve:   LCD: Restricted Values for :     LCD =  Restricted Values: (because is in the denominator)  Step 1: Multiply every term by   Step 2: Simplify   Step 3: Solve     Since , this is a valid solution.  Check:       Therefore, the solution is .   "
+},
+{
+  "id": "ex-denominator-2",
+  "level": "2",
+  "url": "section-1-1.html#ex-denominator-2",
+  "type": "Example",
+  "number": "4",
+  "title": "",
+  "body": "  Solve:   LCD: Restricted Values for :     LCD =  Restricted Values: and  Step 1: Multiply every term by   Step 2: Simplify each term         Step 3: Factor     Since is a restricted value (it makes ), we discard it.  Therefore, the solution is:    "
+},
+{
   "id": "activity-01-intro-activity",
   "level": "1",
   "url": "activity-01-intro-activity.html",
@@ -53,6 +98,51 @@ var ptx_lunr_docs = [
   "number": "1",
   "title": "",
   "body": "  This is the first exercise.   "
+},
+{
+  "id": "activity",
+  "level": "1",
+  "url": "activity.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Activity Title",
+  "body": " Activity Title    This is the introduction to the activity.      This is the first exercise. The derivative of the integral is:       Testing testing.     testing proof     Let be a prime and let all matrices be over . Suppose and are such that If there exist such that with the conditions where  is the last row of (restricted to its first entries),  is the last column of (restricted to its first entries),  is the top-left submatrix of ,  is the top-left submatrix of ,  then is a Calogero-Moser pair.  Conversely, if is a Calogero-Moser pair such that with and , then for any and satisfying there exists such that     Sketch of the proof:   Block notation. Write   Set    Then we have the following commutators:    In addition with Moreover,   Now, since and , we have   Using the explicit block commutators, the equation is equivalent to   Taking the top-left block gives   This yields the Calogero-Moser condition for , precisely when .    "
+},
+{
+  "id": "activity-3",
+  "level": "2",
+  "url": "activity.html#activity-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  This is the first exercise. The derivative of the integral is:    "
+},
+{
+  "id": "activity-4",
+  "level": "2",
+  "url": "activity.html#activity-4",
+  "type": "Theorem",
+  "number": "5",
+  "title": "",
+  "body": "  Testing testing.   "
+},
+{
+  "id": "activity-5",
+  "level": "2",
+  "url": "activity.html#activity-5",
+  "type": "Proof",
+  "number": "1",
+  "title": "",
+  "body": " testing proof  "
+},
+{
+  "id": "activity-6",
+  "level": "2",
+  "url": "activity.html#activity-6",
+  "type": "Proposition",
+  "number": "6",
+  "title": "",
+  "body": "  Let be a prime and let all matrices be over . Suppose and are such that If there exist such that with the conditions where  is the last row of (restricted to its first entries),  is the last column of (restricted to its first entries),  is the top-left submatrix of ,  is the top-left submatrix of ,  then is a Calogero-Moser pair.  Conversely, if is a Calogero-Moser pair such that with and , then for any and satisfying there exists such that     Sketch of the proof:   Block notation. Write   Set    Then we have the following commutators:    In addition with Moreover,   Now, since and , we have   Using the explicit block commutators, the equation is equivalent to   Taking the top-left block gives   This yields the Calogero-Moser condition for , precisely when .   "
 },
 {
   "id": "handouts",
